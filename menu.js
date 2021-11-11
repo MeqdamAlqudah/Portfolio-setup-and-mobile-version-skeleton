e86dd217fa1e5cad798bd760305ca5ebf4646d71
MeqdamAlqudah <memuq56@gmail.com>
2021-11-11 09:11:33 +0200

    Fix speliing error

--- menu.js
+++ menu.js
@@ -236,8 +236,8 @@ const errorElement = document.getElementById('error');
 // Validate-contact-form function
 const errorPresent = function (e) {
   if (email.value !== email.value.toLowerCase()) {
-    errorElement.innerText = "Please enter the email with lower case letter's";
     e.preventDefault();
+    errorElement.innerText = "Please enter the email with lower case letter's";
   }
 };
 
