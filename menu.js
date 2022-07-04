27506848fa1133073e37d7dbec984a55a39f91eb
MeqdamAlqudah <memuq56@gmail.com>
2022-07-04 16:17:18 +0300

    Make the portifolio more responsive + add some projects to it

--- menu.js
+++ menu.js
@@ -10,6 +10,9 @@ const callsClose = function () {
   document.getElementsByClassName("geom")[0].classList.remove("hidden"); // Show the about myself
   document.querySelector("body").style.width = lastWidth; // return the last width of the body
   document.querySelector("body").style.height = lastHeight;
+  document.getElementsByClassName("Works")[0].style.display = "block"
+  document.querySelector("body").style.backgroundColor = "white"
+  document.querySelector(".container_2").style.justifyItems = "initial"
 };
 
 const callsOpen = function () {
@@ -18,8 +21,11 @@ const callsOpen = function () {
   document.getElementsByClassName("headline")[0].classList.add("hidden"); // hide the headline to controll the popup size as we want
   document.getElementsByClassName("Desktop-head")[0].classList.add("hidden"); // hide the Desktop head img to controll the popup size as we want
   document.getElementsByClassName("geom")[0].classList.add("hidden"); // hide the geom img to controll the popup size as we want
-  document.querySelector("body").style.width = "10vmax"; // shrink the size of the body to controll the popup size as we want
-  document.querySelector("body").style.height = "1vmax";
+  document.querySelector("body").style.width = "0"; // shrink the size of the body to controll the popup size as we want
+  document.querySelector("body").style.height = "0";
+  document.getElementsByClassName("Works")[0].style.display = "flex"; // return the work section
+  document.querySelector("body").style.backgroundColor = "#1a2236"
+  document.querySelector(".container_2").style.justifyItems = "center"
 };
 /* close and open the menu */
 // in style.css you can find i pul the display property for the hidden class to none
@@ -44,8 +50,9 @@ const openMenu = function () {
 };
 // popup1
 const openPopup1 = function () {
-  document.getElementsByClassName("popup-1")[0].classList.remove("hidden"); // Show popup-1
   callsOpen();
+  document.getElementsByClassName("popup-1")[0].classList.remove("hidden"); // Show popup-1
+
 };
 
 const closePopup1 = function () {
@@ -180,35 +187,62 @@ document.getElementById("SeeD6").addEventListener("click", openPopup2);
 document
   .getElementsByClassName("cancel-6")[0]
   .addEventListener("click", closePopup6);
+
+// Change the image in the popup window
+let currentImage =   0
+
+const changeScreeshoutIncrease = (currentPopup)=>{
+  if (currentImage > 4) {
+    currentImage = -1
+  }
+  document.querySelector(`.popup-${currentPopup + 1} .popupimgmac`).setAttribute('src',storeObj[currentPopup + 1].featuredImg[currentImage>=3?currentImage=0:currentImage+=1]);
+ for(let i =1;i<=4;i++){
+  document.querySelector(`.popup-${currentPopup + 1} .pimg-${i} img`).classList.remove("choosenImage")
+ }
+  document.querySelector(`.popup-${currentPopup + 1} .pimg-${currentImage + 1} img`).classList.add("choosenImage")
+
+}
+const changeScreeshoutDecrease = (currentPopup)=>{
+  if (currentImage < 0) {
+    currentImage = 5
+  }
+
+  document.querySelector(`.popup-${currentPopup + 1} .popupimgmac`).setAttribute('src',storeObj[currentPopup + 1].featuredImg[currentImage<=0?currentImage=3:currentImage-=1]);
+  for(let i =1;i<=4;i++){
+    document.querySelector(`.popup-${currentPopup + 1} .pimg-${i} img`).classList.remove("choosenImage")
+   }
+    document.querySelector(`.popup-${currentPopup + 1} .pimg-${currentImage + 1} img`).classList.add("choosenImage")
+
+}
 // Storing the Data
 const text =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\r\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.";
 const storeObj = {
   1: {
-    name: "Project name goes here",
-    description: text,
+    name: "Book-mechanic",
+    description: "Book-mechanic is a web appliacation that allow the user to choose a car and set an appointment for each one to fix it and attach it to a mechanic and a city name",
     featuredImg: [
-      "img/Popup-menu/Mac-pop-2.png",
-      "img/Popup-menu/Mac-pop-3.png",
-      "img/Popup-menu/Mac-pop-4.png",
-      "img/Popup-menu/Mac-pop-5.png",
+      "img/Popup-menu/popup-1/1.png",
+      "img/Popup-menu/popup-1/2.png",
+      "img/Popup-menu/popup-1/3.png",
+      "img/Popup-menu/popup-1/4.png",
     ],
     technologies: ["HTML/Css", "Ruby on Rails", "JavaScript"],
     live: "#",
     source: "#",
   },
   2: {
-    name: "Project name goes here",
-    description: text,
+    name: "Air Polution",
+    description: "Air Polution is an application that allow the user to choose a contenent then choose a country in this contentent then the user can see data about the air gas in this country ",
     featuredImg: [
-      "img/Popup-menu/Mac-pop-2.png",
-      "img/Popup-menu/Mac-pop-3.png",
-      "img/Popup-menu/Mac-pop-4.png",
-      "img/Popup-menu/Mac-pop-5.png",
+      "img/Popup-menu/popup-3/1.png",
+      "img/Popup-menu/popup-3/2.png",
+      "img/Popup-menu/popup-3/3.png",
+      "img/Popup-menu/popup-3/4.png",
     ],
-    technologies: ["HTML/Css", "Ruby on Rails", "JavaScript"],
-    live: "#",
-    source: "#",
+    technologies: ["HTML/Css", "JavaScript", "React.js"],
+    live: "https://brave-dubinsky-944f96.netlify.app/",
+    source: "https://github.com/MeqdamAlqudah/Air-polution",
   },
   3: {
     name: "Project name goes here",
@@ -265,10 +299,13 @@ const storeObj = {
 };
 const objValues = Object.keys(storeObj);
 for (let i = 0; i < objValues.length; i += 1) {
+
   document.querySelector(`.popup-${i + 1} p`).textContent =
     storeObj[i + 1].description;
   document.querySelector(`.popup-${i + 1} h5`).textContent =
     storeObj[i + 1].name;
+  document.querySelector(".popupimgmac").setAttribute('src',storeObj[1].featuredImg[0]);
+
   for (let j = 0; j < storeObj[i + 1].featuredImg.length; j += 1) {
     document
       .querySelector(`.popup-${i + 1} .pimg-${j + 1} img`)
@@ -285,6 +322,9 @@ for (let i = 0; i < objValues.length; i += 1) {
   document
     .querySelector(`.popup-${i + 1} .source`)
     .setAttribute("href", storeObj[i + 1].source);
+    document.querySelector(`.popup-${i + 1} .vector-1`).addEventListener("click",()=>changeScreeshoutDecrease(i));
+    document.querySelector(`.popup-${i + 1} .vector-2`).addEventListener("click",()=>changeScreeshoutIncrease(i));
+
 }
 /* Validate-contact-form */
 const email = document.getElementById("mail");
@@ -322,17 +362,57 @@ const hover = function () {
       "block";
   }
 };
-document.querySelector("body").addEventListener("scroll", hover);
+window.addEventListener("scroll", (e)=>{
+const headlineRect = document.querySelector(".headline").getBoundingClientRect()
+const portfolio = document.querySelector(".Works").getBoundingClientRect()
+const About = document.querySelector(".About-myself").getBoundingClientRect()
+const contact = document.querySelector("footer").getBoundingClientRect()
+
+const yCoordinate = window.pageYOffset;
+if (headlineRect.y +500 >=0){
+  document.querySelector("#head-1").classList.add("hover");
+  document.querySelector("#head-2").classList.remove("hover");
+  document.querySelector("#head-3").classList.remove("hover");
+  document.querySelector("#head-4").classList.remove("hover");
+}else if(portfolio.y + 900>=0) {
+  document.querySelector("#head-1").classList.remove("hover");
+  document.querySelector("#head-2").classList.add("hover");
+  document.querySelector("#head-3").classList.remove("hover");
+  document.querySelector("#head-4").classList.remove("hover");
+}else if ( About.y >=0) {
+  document.querySelector("#head-1").classList.remove("hover");
+  document.querySelector("#head-2").classList.remove("hover");
+  document.querySelector("#head-3").classList.add("hover");
+  document.querySelector("#head-4").classList.remove("hover");
+}else if(contact.y >= 0) {
+  document.querySelector("#head-1").classList.remove("hover");
+  document.querySelector("#head-2").classList.remove("hover");
+  document.querySelector("#head-3").classList.remove("hover");
+  document.querySelector("#head-4").classList.add("hover");
+}
+
+});
 
 // preserve the last entered in the form
 form.addEventListener("submit", errorPresent);
 const formId = JSON.parse(localStorage.getItem("formId"));
-if (!document.getElementById("mail").value) {
+if (!document.getElementById("mail").value && (localStorage.length>0)) {
   document.getElementById("mail").value = formId.emailId;
 }
-if (!document.getElementById("name").value) {
+if (!document.getElementById("name").value && (localStorage.length>0)) {
   document.getElementById("name").value = formId.userNameId;
 }
-if (!document.getElementById("Message").value) {
-  document.getElementById("Message").value = formId.messageId;
+if (!document.getElementById("Message").value && (localStorage.length>0)) {
+  document.getElementByfId("Message").value = formId.messageId;
 }
+
+const popupArray = ["first","second","third","fourth","fifth","six"]
+// for(i=0;i<= 5;i++){
+//   document.querySelector(`.dropup_${i+1}`).style.width = `${document.querySelector(`#${popupArray[i]}-image`).clientWidth}px`
+//  console.log(`#${popupArray[i]}-image`,document.querySelector(`#${popupArray[i]}-image`).clientWidth);
+//   // document.querySelector(`.dropup_${i+1}`).style.width = "2px"
+// }
+// console.log(popupArray)
+
+
+
